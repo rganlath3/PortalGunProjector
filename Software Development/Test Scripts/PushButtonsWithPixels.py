4742846fb36b525f4feff0cc82e9d62452fc64aa
rganlath3 <ranil.ganlath@gmail.com>
2024-03-20 00:06:05 -0600

    Updated for longer LED chain

--- Software Development/Test Scripts/PushButtonsWithPixels.py
+++ Software Development/Test Scripts/PushButtonsWithPixels.py
@@ -7,23 +7,32 @@ from signal import pause
 
 #Wire Up NeoPixels and Buttons to 5V.
 
-
 def callback_BlueButton_Pressed():
     print("Shooting Blue Portal!")
-    FWD_Pixel[0] = (0, 0, 255); #Blue
-    AFT_Pixel[0] = (0, 0, 255); #Blue
+    setAllBlueStatic()
 
 def callback_OrangeButton_Pressed():
     print("Shooting Orange Portal!")
-    FWD_Pixel[0] = (255, 30, 0); #Orange
-    AFT_Pixel[0] = (255, 30, 0); #Orange
+    setAllOrangeStatic()
+
+def setAllOrangeStatic():
+    FWD_Pixel[0] = (255, 30, 0) #Orange
+    for x in range(0,28):
+        AFT_Pixel[x]=(255,30,0)
+        
+
+def setAllBlueStatic():
+    FWD_Pixel[0] = (0, 0, 255) #Blue
+    for x in range(0,28):
+        AFT_Pixel[x]=(0,0,255)
+        
 
 
 BlueButton = Button(17) #Blue Pushbutton
 OrangeButton = Button(15) #Orange Pushbutton
 
 FWD_Pixel = neopixel.NeoPixel(board.D21, 1, brightness=1)
-AFT_Pixel = neopixel.NeoPixel(board.D18, 1, brightness=1)
+AFT_Pixel = neopixel.NeoPixel(board.D18, 29, brightness=1)
 
 
 while True:
